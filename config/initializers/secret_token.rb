0751bb9e80f8c8b4a755ddc7c6e7c31044dc09a3
Brett Bim <osiris43@gmail.com>
2011-07-19 11:23:33 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poolofgreatness::Application.config.secret_token = '920fd4b283c0e412e7ff748400009b77bc1cb05c8809ebf0ec9999e9f482b857cdb0e23fce1cf5657ff92631ce806400a66da09fd3fdce21b3aed914d14c2f61'
