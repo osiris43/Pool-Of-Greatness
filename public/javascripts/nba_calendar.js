a91faddcf3812a6e46ef33f5089f4089b4f497de
Brett Bim <osiris43@gmail.com>
2012-02-13 12:18:08 -0600

    main day navigation for nba calendar

--- public/javascripts/nba_calendar.js
+++ public/javascripts/nba_calendar.js
@@ -16,8 +16,6 @@ CalendarApp.GamedateList = Backbone.Collection.extend({
 CalendarApp.GamedateView = Backbone.View.extend({
   tagName: "li",
 
-  events: { "click" : "addCurrent"},
-
   template: function(){
     return _.template($('#gamedate-template').html()); 
   }, 
@@ -39,11 +37,39 @@ CalendarApp.GamedateView = Backbone.View.extend({
     this.$('.dayname-text').text(day);
     this.$('.daynumber-text').text(num);
     this.$('a').attr('href',this.model.get('href'));
+    var date_param = this.getUrlVars()['schedule_date'];
+    
+    if(date_param === undefined){
+      var today = new Date();
+      if(today.getDate() === this.model.get('num')){
+        this.addCurrent();
+      } 
+    } else{
+      /* console.log('date param matched');
+      console.log(date_param);
+      console.log(this.model.get('sched_date'));  */
+      if(this.model.get('sched_date') === date_param){
+        this.addCurrent();
+      }
+    } 
   },
 
   addCurrent: function(){
     $(this.el).addClass("current");
+  },
+
+  getUrlVars: function(){
+    var vars = [], hash
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++){
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+
+    return vars;
   }
+
 });
 
 CalendarApp.ScheduleView = Backbone.View.extend({
@@ -69,13 +95,13 @@ CalendarApp.ScheduleView = Backbone.View.extend({
     days[6] = "Sat";
 
     for(x=0;x<7;x++){
-      href = "?schedule_date=" + beginWeek.getFullYear() + ("00" + (beginWeek.getMonth() + 1)).slice(-2) + ("00" + (beginWeek.getDate() + x)).slice(-2);
-      var gameDate = new CalendarApp.Gamedate({day : days[x], num : beginWeek.getDate() + x, href : href});
+      schedule_date = beginWeek.getFullYear() + ("00" + (beginWeek.getMonth() + 1)).slice(-2) + ("00" + (beginWeek.getDate() + x)).slice(-2); 
+      href = "?schedule_date=" + schedule_date
+      var gameDate = new CalendarApp.Gamedate({day : days[x], num : beginWeek.getDate() + x, href : href, sched_date : schedule_date});
       var view = new CalendarApp.GamedateView({model : gameDate});
       $(this.el).find('ul').append(view.render().el); 
     }
+  },
 
-    console.log($(this.el).html());
-  }
 })
 
