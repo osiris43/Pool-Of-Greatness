b0d1af7bca7f5167b83a6d0afc992f70b8877948
Brett Bim <osiris43@gmail.com>
2013-03-17 13:28:58 -0400

    image config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,9 +9,6 @@ Poolofgreatness::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
-
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
